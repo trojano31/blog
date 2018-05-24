47f06bae87d3bf539f9e655479de064772131798
trojan <patryk.trojanowski98@gmail.com>
2018-05-24 14:48:18 +0200

    initial

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+const
+    gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    server = require('gulp-server-livereload');
+
+gulp.task("default", ['html', 'sass', 'watch', 'webserver']);
+
+
+//watch
+gulp.task('watch', function(){
+   gulp.watch('./src/assets/scss/**/*.scss', ['sass']);
+   gulp.watch('./src/**/*.html', ['html']);
+});
+
+
+//compile
+gulp.task('sass', function(){
+   return gulp.src('./src/assets/scss/**/*.scss')
+       .pipe(sass.sync().on('error', sass.logError))
+       .pipe(gulp.dest('./dist/css'));
+});
+
+gulp.task('html', function(){
+   return gulp.src('./src/**/*.html')
+       .pipe(gulp.dest('./dist'));
+});
+
+
+//server
+gulp.task('webserver', function(){
+   gulp.src('./dist')
+       .pipe(server({
+           livereload: true,
+           open: true
+       }));
+});
